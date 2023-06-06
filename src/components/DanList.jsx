9d92408551061a310965602172d083cb9768dd91
Tomás Serpez <tomasserpez@gmail.com>
2023-06-06 17:59:58 -0300

    ABD done, M is missing

--- src/components/DanList.jsx
+++ src/components/DanList.jsx
@@ -1,89 +1,95 @@
 import React, { useEffect, useState } from 'react';
 import { useTable, useGlobalFilter } from 'react-table';
 
-
 import { getDanes, deleteDan, updateDan } from '../features/apiCalls';
-
 
 const DanList = () => {
   const [danes, setDanes] = useState([]);
   const [globalFilter, setFilterValue] = useState('');
   const [expandedRowId, setExpandedRowId] = useState(null);
 
-  
   const generateUniqueKey = (prefix) => {
     return `${prefix}-${new Date().getTime()}`;
   };
-  
 
-  const ArrayDanes = ["Shodan", "Nidan", "Sandan", "Yodan","Godan","Rokudan", "Nanadan", "Hachidan"];
+  const ArrayDanes = [
+    "Shodan",
+    "Nidan",
+    "Sandan",
+    "Yodan",
+    "Godan",
+    "Rokudan",
+    "Nanadan",
+    "Hachidan"
+  ];
+
   useEffect(() => {
-    // Simulación de datos obtenidos de la API
     const fetchDanes = async () => {
       const danesData = await getDanes();
       setDanes(danesData);
+      location.refresh();
     };
 
     fetchDanes();
   }, []);
 
   const handleDelete = async (id) => {
-    await deleteDan(id);
-    // Actualizar listado de danes luego de la eliminación de uno...
-    const danesData = await getDanes();
-    setDanes(danesData);
+    // Tiene que preguntar si estamos seguros de que queremos borrar el alumno
+    const deleteConfirm = window.confirm(
+      '¿Estás seguro de que quieres borrar el alumno?'
+    );
+    if (deleteConfirm) {
+      await deleteDan(id);
+      const updatedDanes = danes.filter((d) => d.id !== id);
+      setDanes(updatedDanes);
+      alert("El dan ha sido promocionado correctamente.");
+      location.refresh();
+    }
   };
 
   const handleEdit = (id) => {
-    // Lógica para editar un dan por su ID
-    // ...
     console.log(id);
   };
-  
+
   const handlePromotion = (dan) => {
     const index = ArrayDanes.indexOf(dan.NroDan);
     if (index >= 7) {
       alert("No se puede promocionar un dan que ya está en la posición máxima");
     } else {
-      const updatedDan = { ...dan };
-      updatedDan.NroDan = ArrayDanes[index + 1];
-      updateDan(updatedDan.id, updatedDan)
-        .then(() => {
-          setDanes((prevDanes) => {
-            const danIndex = prevDanes.findIndex((d) => d.id === dan.id);
-            const updatedDanes = [...prevDanes];
-            updatedDanes.splice(danIndex, 1);
-            updatedDanes.splice(danIndex, 0, updatedDan);
-            return updatedDanes;
-          });
-          alert("El alumno ha sido promocionado correctamente.");
-        })
-        .catch((error) => {
-          console.log("Error al promocionar al alumno: ", error);
-          alert("Ha ocurrido un error al promocionar al alumno.");
-        });
+      const promotionConfirm = window.confirm(
+        '¿Estás seguro de que quieres a ' + dan.NombreApellido + ' promocionar de' + dan.NroDan + ' a ' + ArrayDanes[index + 1] +  '?'
+      );
+      if (promotionConfirm) {
+        const newDan = {
+          ...dan,
+          NroDan: ArrayDanes[index + 1]
+        };
+        updateDan(newDan);
+        const updatedDanes = danes.map((d) => (d.id === newDan.id ? newDan : d));
+        setDanes(updatedDanes);
+        alert("El dan ha sido promocionado correctamente.");
+      }
     }
   };
-  
-  
+
   const handleExpand = (id) => {
     setExpandedRowId((prevId) => (prevId === id ? null : id));
   };
 
   const columns = React.useMemo(
     () => [
-        {
-          Header: 'ID',
-          accessor: 'id',
-        },
-        {
-          Header: 'Nombre y Apellido',
-          accessor: 'NombreApellido',
-        },
-        {
-          Header: 'DNI',
-          accessor: 'dni',
-        },
+      {
+        Header: 'ID',
+        accessor: 'id',
+      },
+      {
+        Header: 'Nombre y Apellido',
+        accessor: 'NombreApellido',
+      },
+      {
+        Header: 'DNI',
+        accessor: 'dni',
+      },
       {
         Header: 'Nro Dan',
         accessor: 'NroDan',
@@ -91,30 +97,30 @@ const DanList = () => {
       {
         Header: 'Dojo al que responde',
         accessor: 'QueDojoPertenece',
-          },
+      },
       {
         Header: 'Fecha del último examen',
         accessor: 'FechaUltimoExamen',
       },
-      {
-        Header: 'Tipo de Alumno',
-        accessor: 'TipoDeAlumno',
+      {  
+        Header: 'Provincia',
+        accessor: 'Provincia',
       },
       {
         Header: 'Acciones',
         Cell: ({ row }) => (
-          <div>
+          <div className="flex flex-row">
             <button
               onClick={() => handleDelete(row.original.id)}
-              key={generateUniqueKey('delete')} // Generar key única
-              className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded mr-2"
+              key={generateUniqueKey('delete')}
+              className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 m-1  rounded mr-2"
             >
               Eliminar
             </button>
             <button
-              key={generateUniqueKey('edit')} // Generar key única
+              key={generateUniqueKey('edit')}
               onClick={() => handleEdit(row.original.id)}
-              className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+              className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 m-1 rounded"
             >
               Editar
             </button>
@@ -135,13 +141,13 @@ const DanList = () => {
           </div>
         ),
       }
-      
-    ],[]
+    ],
+    []
   );
 
   const {
     getTableProps,
-    getTableBodyProps,  
+    getTableBodyProps,
     headerGroups,
     rows,
     prepareRow,
@@ -153,109 +159,117 @@ const DanList = () => {
     setFilterValue(e.target.value);
   };
 
-return (
-  <div className="overflow-x-auto">
-    <input
-      value={globalFilter}
-      onChange={handleFilterChange}
-      placeholder="Buscar dan..."
-      className="w-64 rounded-xl border p-2 mb-4"
-    />
-    <table {...getTableProps()} className="w-full">
-      <thead>
-        {headerGroups.map((headerGroup) => (
-          <tr {...headerGroup.getHeaderGroupProps()}>
-            {headerGroup.headers.map((column) => (
-              <th
-                {...column.getHeaderProps()}
-                className="px-4 py-2 bg-gray-100 text-center text-gray-700 font-bold"
-              >
-                {column.render('Header')}
-              </th>
-            ))}
-          </tr>
-        ))}
-      </thead>
-      <tbody {...getTableBodyProps()}>
-        {rows.map((row) => {
-          prepareRow(row);
-          const isExpanded = row.original.id === expandedRowId;
-          return (
-            <>
-            <React.Fragment key={`${generateUniqueKey('row')}-${row.original.id}`}> {/* Asignar una key única al fragmento */}
-              <tr {...row.getRowProps()}>
-                {row.cells.map((cell) => {
-                  if (
-                    isExpanded &&
-                    ![
-                      'id',
-                      'NombreApellido',
-                      'dni',
-                      'NroDan',
-                      'Nacionalidad',
-                      'Observacion',
-                      'QueDojoPertenece',
-                      'FechaUltimoExamen',
-                      'TipoDeAlumno',
-                      'Acciones',
-                      'Promocionar',
-                    ].includes(cell.column.id)
-                  ) {
-                    return null; // Ocultar las celdas no deseadas
-                  }
-                  return (
+  return (
+    <div className="overflow-x-auto">
+      <input
+        value={globalFilter}
+        onChange={handleFilterChange}
+        placeholder="Buscar dan..."
+        className="w-64 rounded-xl border p-2 mb-4"
+      />
+      <table {...getTableProps()} className="w-full">
+        <thead>
+          {headerGroups.map((headerGroup) => (
+            <tr {...headerGroup.getHeaderGroupProps()}>
+              {headerGroup.headers.map((column) => (
+                <th
+                  {...column.getHeaderProps()}
+                  className="px-4 py-2 bg-gray-100 text-center text-gray-700 font-bold"
+                >
+                  {column.render('Header')}
+                </th>
+              ))}
+              <th className="px-4 py-2 bg-gray-100"></th> {/* Espacio para el botón "Mostrar" */}
+            </tr>
+          ))}
+        </thead>
+        <tbody {...getTableBodyProps()}>
+          {rows.map((row) => {
+            prepareRow(row);
+            const isExpanded = row.original.id === expandedRowId;
+            return (
+              <>
+                <tr {...row.getRowProps()}>
+                  {row.cells.map((cell) => (
                     <td
                       {...cell.getCellProps()}
                       className="px-4 py-2 border-b border-gray-200"
                     >
                       {cell.render('Cell')}
                     </td>
-                  );
-                })}
-                <td>
-                  <button
-                    onClick={() => handleExpand(row.original.id)}
-                    className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
-                  >
-                    {isExpanded ? 'Ocultar' : 'Mostrar'}
-                  </button>
-                </td>
-              </tr>
-              {isExpanded && (
-                <tr key={`${generateUniqueKey('tr')}`}>
-                  <td colSpan={columns.length + 1}>
-                    {/* Renderizar información oculta */}
-                    <div className="text-left">
-                      <p><strong>Número de miembro:</strong> {row.original.NroMiembro}</p>
-                      <p><strong>Nacionalidad:</strong> {row.original.Nacionalidad}</p>
-                      <p><strong>Fecha de Nacimiento:</strong> {row.original.FechaNacimiento}</p>
-                      <p><strong>Número de AF:</strong> {row.original.NroAF}</p>
-                      <p><strong>Fecha del proximo examen:</strong> {row.original.FechaProximoExamen}</p>
-                      <p><strong>Contacto:</strong></p>
-                      <ul className="pl-2">
-                        <li><strong>Teléfono:</strong> {row.original.Telefono}</li>
-                        <li><strong>Email:</strong> {row.original.Email}</li>
-                      </ul>
-                      <p><strong>Direccion:</strong> {row.original.Direccion}</p>
-                      <p><strong>Codigo Postal:</strong> {row.original.CodigoPostal}</p>
-                      <p><strong>Observación:</strong> {row.original.Observacion}</p>
-                      {/* Agrega aquí el resto de los campos ocultos */}
-                    </div>
+                  ))}
+                  <td className="px-4 py-2 border-b border-gray-200">
+                    <button
+                      onClick={() => handleExpand(row.original.id)}
+                      className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 m-2 rounded"
+                    >
+                      {isExpanded ? 'Ocultar' : 'Mostrar'}
+                    </button>
                   </td>
                 </tr>
-                
-              )}
-              </React.Fragment>
-
-            </>
-          );
-        })}
-      </tbody>
-    </table>
-  </div>
-);
-
-  
+                {isExpanded && (
+                  <tr key={`${generateUniqueKey('tr')}`}>
+                    <td colSpan={columns.length + 1}>
+                      <div className="text-left p-5">
+                        <p>
+                          <strong>Número de miembro: </strong>{' '}
+                          {row.original.NroMiembro}
+                        </p>
+                        <p>
+                          <strong>Nacionalidad: </strong>{' '}
+                          {row.original.Nacionalidad}
+                        </p>
+                        <p>
+                          <strong>Fecha de Nacimiento: </strong>{' '}
+                          {row.original.FechaNacimiento}
+                        </p>
+                        <p>
+                          <strong>Número de AF: </strong>{' '}
+                          {row.original.NroAF}
+                        </p>
+                        <p>
+                          <strong>Fecha del proximo examen: </strong>{' '}
+                          {row.original.FechaProximoExamen}
+                        </p>
+                        <p>
+                          <strong>Contacto: </strong>
+                        </p>
+                        <ul className="pl-5">
+                          <li>
+                            <strong>Teléfono: </strong>{' '}
+                            {row.original.Telefono}
+                          </li>
+                          <li>
+                            <strong>Email: </strong> {row.original.Email}
+                          </li>
+                        </ul>
+                        <p>
+                          <strong>Direccion: </strong>{' '}
+                          {row.original.Direccion}
+                        </p>
+                        <p>
+                          <strong>Codigo Postal: </strong>{' '}
+                          {row.original.CodigoPostal}
+                        </p>
+                        <p>
+                          <strong>Tipo de Alumno: </strong>
+                          {row.original.TipoDeAlumno}
+                        </p>
+                        <p>
+                          <strong>Observación: </strong>{' '}
+                          {row.original.Observacion}
+                        </p>
+                      </div>
+                    </td>
+                  </tr>
+                )}
+              </>
+            );
+          })}
+        </tbody>
+      </table>
+    </div>
+  );
 };
 
 export default DanList;
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,16 @@
 import React, { useState } from 'react'
 
 const Navbar = () => {
-  // Definimos la variable y función de seteo que usaremos para determinar si el navbar está abierto o cerrado
-  const [isOpen, setIsOpen] = useState(false);
+  // Determinamos si estamos parados en CREAR/EDITAR o en Listar
+  const handleRedirect = () => {
+    // si estamos en /crear el boton debe decir "Volver" y redirigirnos a la pantalla de inicio
+    if (window.location.pathname === '/crear') {
+      window.location.href = '/'
+    }
+    else{
+      window.location.href = '/crear'
+    }
+  }
 
   return (
     <>
@@ -14,8 +22,9 @@ const Navbar = () => {
         </div>
         {/* El boton para dar de alta que se vea a la derecha en lg */}
         <div className="mt-4 lg:mt-0">
-          <button className="inline-flex items-center bg-amber-500 border-0 py-2 px-4 text-white hover:bg-amber-600 focus:outline-none rounded hover:animate-pulse">
-            Dar de alta
+          <button className="inline-flex items-center bg-amber-500 border-0 py-2 px-4 text-white hover:bg-amber-600 focus:outline-none rounded hover:animate-pulse"
+          onClick={() => handleRedirect()}>
+            {window.location.pathname === '/crear' ? 'Volver' : 'Crear'}
           </button>
         </div>
       </nav>
